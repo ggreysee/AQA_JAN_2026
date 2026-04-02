18f4ee6e6cd6a2346679e167de3d2aa06b3991e3
ggreysee <ggreysee@gmail.com>
2026-04-02 16:01:21 +0200

    Cucumber22

---
+++ src/test/java/org/prog/session21HW/steps/MyDBSteps2.java
@@ -0,0 +1,62 @@
+package org.prog.session21HW.steps;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class MyDBSteps2 {
+    public static Connection connection;
+
+    @When("I check these phones in DB")
+    public void checkPhonesInDB() throws SQLException {
+        for (int i = 0; i < MyGoogleSteps.models.size(); i++) {
+            PreparedStatement preparedStatement = connection.prepareStatement(
+                    "SELECT * FROM phones WHERE model = ?"
+            );
+            preparedStatement.setString(1, MyGoogleSteps.models.get(i));
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                String dbPrice = resultSet.getString("price");
+                if (!MyGoogleSteps.prices.get(i).equals(dbPrice)) {
+                    throw new RuntimeException("Price mismatch");
+                }
+            } else {
+                System.out.println("ENTER ELSE");
+                PreparedStatement insertStatement = connection.prepareStatement(
+                        "INSERT INTO phones (model, price)" +
+                                "VALUES (?, ?);"
+                );
+                insertStatement.setString(1, MyGoogleSteps.models.get(i));
+                insertStatement.setString(2, MyGoogleSteps.prices.get(i));
+                insertStatement.execute();
+                System.out.println("INSERT DONE");
+
+            }
+        }
+
+    }
+
+    @Then("Prices should match or be saved")
+    public void checkResult() throws SQLException {
+        for (int i = 0; i < MyGoogleSteps.models.size(); i++) {
+            PreparedStatement preparedStatement = connection.prepareStatement(
+                    "SELECT * FROM phones WHERE model = ?"
+            );
+            preparedStatement.setString(1, MyGoogleSteps.models.get(i));
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (!resultSet.next()) {
+                throw new RuntimeException("Phone not found in DB");
+            }
+
+            String dbPrice = resultSet.getString("price");
+            if (!MyGoogleSteps.prices.get(i).equals(dbPrice))
+                throw new RuntimeException("Price mismatch");
+        }
+
+    }
+}
+
